f47a46da31eedc92bfa055bb2c69a942466852cf
Alberto <proyectos@albertogomp.es>
2019-03-29 10:00:39 +0100

    Added DAO
    Auto-Update on proyect made at the Office.

---
+++ ControlVersiones/WebTestingProject/source/com/testing/dao/InterfaceDAO.java
@@ -0,0 +1,34 @@
+package com.testing.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.testing.model.SecureLogin;
+
+public interface InterfaceDAO {
+
+	Session openCurrentSession();
+
+	Session openCurrentSessionwithTransaction();
+
+	void closeCurrentSession();
+
+	void closeCurrentSessionwithTransaction();
+
+	Session getCurrentSession();
+
+	void setCurrentSession(Session currentSession);
+
+	Transaction getCurrentTransaction();
+
+	void setCurrentTransaction(Transaction currentTransaction);
+
+	SecureLogin buscarUsuario(String user);
+
+	List<SecureLogin> findAll();
+
+	SecureLogin CheckSecurelogin(String user);
+
+}
---
+++ ControlVersiones/WebTestingProject/source/com/testing/dao/TestingDAO.java
@@ -0,0 +1,137 @@
+package com.testing.dao;
+
+import com.testing.model.*;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+/*
+ * @autor: Alberto Gömez Peña
+ * @web: www.albertogomp.es
+ */
+
+public class TestingDAO implements InterfaceDAO  {
+	private Session currentSession;
+	private Transaction currentTransaction;
+
+	public TestingDAO() {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#openCurrentSession()
+	 */
+	@Override
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#openCurrentSessionwithTransaction()
+	 */
+	@Override
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#closeCurrentSession()
+	 */
+	@Override
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#closeCurrentSessionwithTransaction()
+	 */
+	@Override
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+
+	private static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration()
+			 .configure()
+			 .addPackage("com.testing.model")
+			 .addAnnotatedClass(SecureLogin.class);
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+ 		return sessionFactory;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#getCurrentSession()
+	 */
+	@Override
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#setCurrentSession(org.hibernate.Session)
+	 */
+	@Override
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#getCurrentTransaction()
+	 */
+	@Override
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#setCurrentTransaction(org.hibernate.Transaction)
+	 */
+	@Override
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	} 
+	
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#buscarUsuario(java.lang.String)
+	 */
+	@Override
+	public SecureLogin buscarUsuario(String user) {
+		SecureLogin login = getCurrentSession().get(SecureLogin.class, user);
+
+		return login;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#findAll()
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<SecureLogin> findAll() {
+		 List<SecureLogin> users = (List<SecureLogin>) getCurrentSession().createQuery("from SecureLogin").list() ;
+		return users;
+	}
+
+//	public void deleteAll() {
+//		List<Competidor> entityList = findAll();
+//		for (Competidor entity : entityList) {
+//			delete(entity);
+//		}
+//	}
+
+	/* (non-Javadoc)
+	 * @see com.testing.dao.InterfaceDAO#CheckSecurelogin(java.lang.String)
+	 */
+	@Override
+	public SecureLogin CheckSecurelogin(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
